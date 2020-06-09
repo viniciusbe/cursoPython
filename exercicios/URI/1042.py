a5bbd21a889b9111a877a45a47a2d766962ecaea
viniciusbe <vinicius.bernardesds@gmail.com>
2020-06-09 20:24:55 -0300

    uri

--- exercicios/URI/1042.py
+++ exercicios/URI/1042.py
@@ -2,8 +2,32 @@ a, b, c = map(int, input().split())
 
 if a < b:
     if a < c:
-        print(a)
-    elif c < b:
+        if b < c:
+            print(a)
+            print(b)
+            print(c)
+        else:
+            print(a)
+            print(c)
+            print(b)
+    else:
         print(c)
         print(a)
         print(b)
+elif b < c:
+    if a < c:
+        print(b)
+        print(a)
+        print(c)
+    else:
+        print(b)
+        print(c)
+        print(a)
+else:
+    print(c)
+    print(b)
+    print(a)
+print("")
+print(a)
+print(b)
+print(c)
